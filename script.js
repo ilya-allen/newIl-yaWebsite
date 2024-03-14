a8ebc9f823e34b1f0097cf5137cade622ecc0c03
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-03-14 18:36:46 +1300

    big banner update

--- script.js
+++ script.js
@@ -2,7 +2,18 @@
 
 const homeButton = document.querySelector('.home_button');
 const glassBox = document.querySelector('.glass_box');
+const scrollBanner = document.querySelector('.scroll_banner')
+const bannerText = document.querySelector('.banner_text');
 
 homeButton.addEventListener('click', function() {
     glassBox.style.height = '0px'
+    homeButton.style.transform = 'translateX(-700px) rotateZ(90deg)'
+    homeButton.style.opacity = '0%';
+
+    setTimeout(function() {
+        scrollBanner.style.height = '900px';
+        bannerText.style.fontSize = '110px'
+        homeButton.style.width = '1px'
+    }, 500)
+
 })
