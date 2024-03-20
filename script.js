9f5d7604d567a01719feafe66ab0ea65b4b0e3f8
Il'ya Allen <il.yaallen2008@yahoo.com.au>
2024-03-20 19:25:56 +1300

    intro page

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const homeButton = document.querySelector('.home_button');
 const glassBox = document.querySelector('.glass_box');
 const scrollBanner = document.querySelector('.scroll_banner')
 const bannerText = document.querySelector('.banner_text');
+const introPage = document.querySelector('.intro_page')
 
 homeButton.addEventListener('click', function() {
     glassBox.style.height = '0px'
@@ -11,6 +12,7 @@ homeButton.addEventListener('click', function() {
     homeButton.style.opacity = '0%';
 
     setTimeout(function() {
+        introPage.style.opacity = 1;
         scrollBanner.style.height = '900px';
         bannerText.style.opacity = '100%'
         homeButton.style.width = '1px'
